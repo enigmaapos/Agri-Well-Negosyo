b73d8ae45c688218d817b61be18a4261756d94e1
enigmaapos <apostoliaenigma@gmail.com>
2025-08-02 01:26:19 +0000

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <main className="pt-20"> {/* add top padding so content isn't hidden behind navbar */}
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
