9076de6b90881cf736cf381d6a617eac2f32e728
enigmaapos <apostoliaenigma@gmail.com>
2025-08-02 02:49:41 +0000

    initial commit

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-// components/Footer.js
-export default function Footer() {
-  return (
-    <footer className="bg-green-800 text-white py-8 px-4 text-center">
-      <p>&copy; {new Date().getFullYear()} Agri-Well Negosyo. All rights reserved.</p>
-      <p className="text-sm mt-2">Connect with us: agriwell@example.com</p>
-    </footer>
-  );
-}
