740c6c90002f42d91760e6c060ae12348b8c2097
enigmaapos <apostoliaenigma@gmail.com>
2025-08-02 10:30:57 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Features from '../components/Features';
+import JoinForm from '../components/JoinForm';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Features />
+      <JoinForm />
+      <Footer />
+    </>
+  );
+}
