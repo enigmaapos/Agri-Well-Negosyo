6e8b758651edf7427c7d87fe918401e4b123eaa3
enigmaapos <apostoliaenigma@gmail.com>
2025-08-02 08:21:53 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import Features from '@/components/Features';
-import JoinForm from '@/components/JoinForm';
-import Footer from '@/components/Footer';
+import Hero from './components/Hero';
+import About from './components/About';
+import Features from './components/Features';
+import JoinForm from './components/JoinForm';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
